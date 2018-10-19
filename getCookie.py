a533117ffb9a28e7b45096903b8756888959ae53
leslie-lss <31202711+leslie-lss@users.noreply.github.com>
2018-10-19 17:43:45 +0800

    Create getCookie.py

---
+++ getCookie.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+import cookielib
+import urllib2
+import requests
+
+url_test = 'https://www.instagram.com/'
+
+#设置保存cookie的文件，同级目录下的cookie.txt
+filename = 'cookie.txt'
+#声明一个MozillaCookieJar对象实例来保存cookie，之后写入文件
+cookie = cookielib.MozillaCookieJar(filename)
+#利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
+handler = urllib2.HTTPCookieProcessor(cookie)
+#通过handler来构建opener
+opener = urllib2.build_opener(handler)
+#创建一个请求，原理同urllib2的urlopen
+response = opener.open(url_test)
+#保存cookie到文件
+cookie.save(ignore_discard=True, ignore_expires=True)
